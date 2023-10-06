df0dfaa8dc0a6042a2224164697d0f519dfbc918
lsmith54426 <lss54426@tatatechnologies.com>
2023-10-06 10:50:27 +0100

    location api Changes Finished

--- src/main/java/com/jlr/ttl/ds/api/controllers/LocationController.java
+++ src/main/java/com/jlr/ttl/ds/api/controllers/LocationController.java
@@ -3,59 +3,81 @@ package com.jlr.ttl.ds.api.controllers;
 import com.jlr.ttl.ds.api.constants.DSConstants;
 import com.jlr.ttl.ds.api.dto.DSResponse;
 import com.jlr.ttl.ds.api.dto.entity.Location;
-import com.jlr.ttl.ds.api.dto.entity.Vehicle;
 import com.jlr.ttl.ds.api.dto.response.DSUserResponse;
+import com.jlr.ttl.ds.api.dto.response.LocationResponse;
+import com.jlr.ttl.ds.api.exception.ServiceBusinessException;
+import com.jlr.ttl.ds.api.exception.data.LocationNotFoundException;
 import com.jlr.ttl.ds.api.exception.data.UserNotFoundException;
-import com.jlr.ttl.ds.api.repositories.LocationRepository;
-import com.jlr.ttl.ds.api.repositories.VehicleRepository;
+import com.jlr.ttl.ds.api.exception.data.VehicleNotFoundException;
 import com.jlr.ttl.ds.api.services.LocationService;
-import com.jlr.ttl.ds.api.services.VehicleService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "/v1/locations")
 @CrossOrigin("http://localhost:4200")
 public class LocationController {
 
-    @Autowired
+
     private LocationService locationService;
 
+    /**
+     * Getting all the locations from database.
+     * @return a list of LocationResponse
+     */
     @GetMapping("/all")
-    public ResponseEntity<List<Location>> getAllLocations() {
-        List<Location> response = locationService.getAllLocationService();
-
-            return new ResponseEntity<>(response, HttpStatus.OK);
-
-
-
+    public ResponseEntity<DSResponse<List<LocationResponse>>> getAllLocations() {
+        List<LocationResponse> locationResponse = null;
+        try{
+            locationResponse = locationService.getAllLocations();
+        }catch (ServiceBusinessException serviceBusinessException) {
+            return new ResponseEntity<>(DSResponse
+                    .<List<LocationResponse>>builder()
+                    .status(DSConstants.STATUS_FAILED)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(DSResponse
+                .<List<LocationResponse>>builder()
+                .status(DSConstants.STATUS_SUCCESS)
+                .results(locationResponse)
+                .build(), HttpStatus.OK);
     }
-    @GetMapping("/byid/{lId}")
-    public ResponseEntity indexVehicle(@PathVariable(value = "lId") String lId) {
-        try {
-            return ResponseEntity.ok(locationService.getLocationByID(lId));
-        } catch (IllegalStateException e) {
-            return new ResponseEntity<>(DSResponse
-                    .<DSUserResponse>builder()
-                    .status(DSConstants.STATUS_FAILED)
-                    .build(), HttpStatus.NOT_FOUND);
-        }
-        catch (IllegalArgumentException illegalArgumentException){
-            return new ResponseEntity<>(DSResponse
-                    .<DSUserResponse>builder()
-                    .status(DSConstants.STATUS_FAILED)
-                    .build(), HttpStatus.BAD_REQUEST);
-        }catch (UserNotFoundException userNotFoundException){
-            return new ResponseEntity<>(DSResponse
-                    .<DSUserResponse>builder()
-                    .status(DSConstants.STATUS_FAILED)
-                    .build(), HttpStatus.NOT_FOUND);
-        }
 
+
+
+
+    /**
+     * Fetch the location by the id passed
+     *
+     * @return Location
+     */
+    @GetMapping("/by_id/{id}")
+    public ResponseEntity<DSResponse<LocationResponse>> getLocationById(@PathVariable(value = "id") String id){
+        LocationResponse locationResponse = null;
+        try {
+            locationResponse = locationService.getLocationByID(id);
+        } catch (ServiceBusinessException serviceBusinessException) {
+            return new ResponseEntity<>(DSResponse
+                    .<LocationResponse>builder()
+                    .status(DSConstants.STATUS_FAILED)
+                    .build(), HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        } catch (LocationNotFoundException locationNotFoundException){
+            return new ResponseEntity<>(DSResponse
+                    .<LocationResponse>builder()
+                    .status(DSConstants.STATUS_FAILED)
+                    .build(), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(DSResponse
+                .<LocationResponse>builder()
+                .status(DSConstants.STATUS_SUCCESS)
+                .results(locationResponse)
+                .build(), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/jlr/ttl/ds/api/dto/entity/Location.java
+++ src/main/java/com/jlr/ttl/ds/api/dto/entity/Location.java
@@ -1,25 +1,24 @@
 package com.jlr.ttl.ds.api.dto.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
-@Entity
-@Table(name = "locations")
 public class Location {
-    @Id
-    @Column(name="id")
-    private String lId;
-    @Column(name = "description")
-    private String lDescription;
+
+    private String id;
+
+    private String description;
+
+
+    private Date endDate;
 
 }
