6b8b4540c1c98b99796f6b6c24ae2677be4eeb04
Charhmwang <charwanghm@gmail.com>
2023-10-09 15:56:08 +0100

    Deleted files not related to this ticket

--- src/main/java/com/jlr/ttl/ds/api/exception/data/LocationTypeNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.jlr.ttl.ds.api.exception.data;
-
-public class LocationTypeNotFoundException extends DataNotFoundException {
-    public LocationTypeNotFoundException(String message) { super(message); }
-}
--- src/main/java/com/jlr/ttl/ds/api/repositories/LocationTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jlr.ttl.ds.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationTypeRepository
-        extends JpaRepository<LocationTypeTable, String> {
-}
